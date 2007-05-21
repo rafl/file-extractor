bcdc9412797b7bf4a94ef98636d36a30d77e0042
Florian Ragwitz <rafl@debian.org>
2007-05-21 20:33:10 +0200

    Initial checkin.

---
+++ lib/File/Extractor.pm
@@ -0,0 +1,101 @@
+package File::Extractor;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+File::Extractor - The great new File::Extractor!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use File::Extractor;
+
+    my $foo = File::Extractor->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Florian Ragwitz, C<< <rafl at debian.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to
+C<bug-file-extractor at rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=File-Extractor>.
+I will be notified, and then you'll automatically be notified of progress on
+your bug as I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc File::Extractor
+
+You can also look for information at:
+
+=over 4
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/File-Extractor>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/File-Extractor>
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=File-Extractor>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/File-Extractor>
+
+=back
+
+=head1 ACKNOWLEDGEMENTS
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2007 Florian Ragwitz, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
+1; # End of File::Extractor
